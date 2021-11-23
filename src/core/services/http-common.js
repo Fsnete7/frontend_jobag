55842984ead8ff2e137c658cac61c5cc8f1ca430
Fsnete <43191130+Fsnete@users.noreply.github.com>
2021-11-23 18:22:26 -0500

    Fixing

--- src/core/services/http-common.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://jobagapi.mybluemix.net/api/v1/",
-    headers: {"Content-type": "application/json"}
-});
--- src/core/services/postulations-api-service.js
+++
@@ -1,8 +0,0 @@
-import http from "./http-common"
-
-class PostulationsApiService{
-    getAll(){
-        return http.get("/postulations");
-    }
-}
-export default new PostulationsApiService();
